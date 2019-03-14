c399b00a9116b268d3a2fe39dd063e139006db42
Roger Chao <rogman@gmail.com>
2019-03-14 14:39:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,7 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
-#provider "vault" {
-#  address = "${var.vault_addr}"
-#}
-
-#data "vault_generic_secret" "gcp_credentials" {
-#  path = "secret/${var.vault_user}/gcp/credentials"
-#}
-
-#resource "vault_auth_backend" "k8s" {
-#  type = "kubernetes"
-#  path = "${var.vault_user}-gke-${var.environment}"
-#  description = "Vault Auth backend for Kubernetes"
-#}
-
 provider "google" {
-  #credentials = "${data.vault_generic_secret.gcp_credentials.data[var.gcp_project]}"
   project     = "${var.gcp_project}"
   region      = "${var.gcp_region}"
 }
@@ -47,20 +32,3 @@ resource "google_container_cluster" "k8sexample" {
   }
 }
 
-#resource "null_resource" "auth_config" {
-#  provisioner "local-exec" {
-#    command = "curl --header \"X-Vault-Token: $VAULT_TOKEN\" --header \"Content-Type: application/json\" --request POST --data '{ \"kubernetes_host\": \"https://${google_container_cluster.k8sexample.endpoint}:443\", \"kubernetes_ca_cert\": \"${chomp(replace(base64decode(google_container_cluster.k8sexample.master_auth.0.cluster_ca_certificate), "\n", "\\n"))}\" }' ${var.vault_addr}/v1/auth/${vault_auth_backend.k8s.path}config"
-#  }
-#}
-
-#resource "vault_generic_secret" "role" {
-#  path = "auth/${vault_auth_backend.k8s.path}role/demo"
-#  data_json = <<EOT
-#  {
-#    "bound_service_account_names": "cats-and-dogs",
-#    "bound_service_account_namespaces": "default",
-#    "policies": "${var.vault_user}",
-#    "ttl": "24h"
-#  }
-#  EOT
-#}
