dc8fd44d2bf4b4ff4470e7eef0e0ab0006d254e2
Roger Chao <rogman@gmail.com>
2019-03-14 18:06:47 -0700

    Update gke-consul-helm.sh

--- kubernetes/deployments/consul-helm/gke-consul-helm.sh
+++ kubernetes/deployments/consul-helm/gke-consul-helm.sh
@@ -9,11 +9,11 @@ sleep 25
 #Add our admin
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 
-#Get the chart
-#git clone https://github.com/hashicorp/consul-helm.git
+Get the chart
+git clone https://github.com/hashicorp/consul-helm.git
 
 #Seed license secret
-kubectl apply -f consul-license.yaml
+#kubectl apply -f consul-license.yaml
 
 #Install the chart
 helm install -f values.yaml ./consul-helm --name consul-helm
